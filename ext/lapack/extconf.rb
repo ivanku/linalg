1827a0904e43c7b17555ddfb8f94027a9108643b
Maxim Pechnikov <parallel588@gmail.com>
2014-01-04 22:08:52 +0400

    fixed complile for ruby 2/2.1

--- ext/lapack/extconf.rb
+++ ext/lapack/extconf.rb
@@ -8,7 +8,7 @@ require 'mkmf'
 require 'enumerator'
 
 module Enumerable
-   def map_with_index 
+   def map_with_index
       arr = []
       each_with_index { |e, i|
          arr << yield(e, i)
@@ -40,15 +40,15 @@ module GenerateHeader
                   File.open(pname) { |pfile|
                      pfile.each { |line|
                         next unless line =~ %r!^extern!
-                        
+
                         # I don't understand why f2c inserts these ftnlen params
                         while line.gsub!(%r!, ftnlen [a-z]+_len\)!, ")") ; end
-                        
+
                         outfile.write line
                      }
                   }
                }
-               
+
                outfile.puts
                outfile.puts "#endif"
             }
@@ -63,7 +63,7 @@ module ScanHeader
    def ctype_canonical_form(ctype)
       ctype.gsub(%r!\*!, " * ").gsub(%r!\s+!, " ").strip
    end
-      
+
    def parse_prototype(s, argnames = true)
       func, args = s.strip.gsub(%r!\s+!, " ").split(%r![\(\)]!)
       ret, name =
@@ -83,7 +83,7 @@ module ScanHeader
       end
       desc
    end
-   
+
    def scan_header_io(io, ppdefs = nil)
       s = io.read
       s.gsub!(%r!^\s*\#.*$!, "")
@@ -102,7 +102,7 @@ module ScanHeader
          yield parse_prototype(func)
       }
    end
-   
+
    def scan_header(header, ppdefs = nil, &block)
       File.open(header) { |file|
          scan_header_io(file, ppdefs, &block)
@@ -115,13 +115,13 @@ module FunctionDB
       db = {}
 
       cppdefs = {
-         "extern" => "",  
-         
+         "extern" => "",
+
          # these are in f2c.h
          "C_f" => "void",
          "Z_f" => "void",
       }
-      
+
       ["include/LAPACK.h", "include/BLAS.h"].each { |filename|
          print "reading #{filename}..."
          ScanHeader.scan_header(filename, cppdefs) { |desc|
@@ -129,7 +129,7 @@ module FunctionDB
          }
          puts "ok"
       }
-      
+
       db
    end
 end
@@ -157,22 +157,22 @@ module Lapack_c
                     "define_lapack_#{letter}")
       }
    end
-   
+
    def write_file(db, filename, funcname)
       File.open("#{filename}.c", "w+") { |file|
          file.puts %{#include "rb_lapack.h"\n\n}
          db.keys.sort.each { |func|
             Lapack_c.write_function(db[func], file)
          }
-         
+
          file.puts "void #{funcname}()\n{\n"
-         
+
          db.keys.sort.each { |func|
-            file.puts <<EOS
+         file.puts <<EOS
     rb_define_singleton_method(rb_cLapack,
                                "#{func.sub(%r!_$!, "")}",
                                #{Lapack_c.rbname(func)},
-                               #{db[func][:args].size}) ;
+                               #{-1}) ;
 EOS
          }
 
@@ -185,22 +185,20 @@ EOS
    end
 
    def write_function(desc, out)
-      rbname = rbname(desc[:name])
-      args = ["VALUE klass"] + desc[:args].map_with_index { |type, i|
-         "VALUE v#{i}"
-      }
-      conv = desc[:args].map { |type|
-         "to_" + case type
-                 when %r!^(\w+)$!
-                    $1
-                 when %r!^(\w+)\s*\*$!
-                    "#{$1}_ptr"
-                 else
-                    raise "unknown arg type in #{desc[:name]}: '#{type}'"
-                 end.gsub(" ","_")
-      }
-      assign = desc[:args].map_with_index { |type, i|
-         "#{type} p#{i} = (#{type})NUM2ULONG(rb_funcall(v#{i}, #{conv[i]}, 0))"
+     rbname = rbname(desc[:name])
+     args = ['int argc', 'VALUE *argv', 'VALUE self']
+     conv = desc[:args].map { |type|
+       "to_" + case type
+               when %r!^(\w+)$!
+                 $1
+               when %r!^(\w+)\s*\*$!
+                 "#{$1}_ptr"
+               else
+                 raise "unknown arg type in #{desc[:name]}: '#{type}'"
+               end.gsub(" ","_")
+     }
+     assign = desc[:args].map_with_index { |type, i|
+       "#{type} p#{i} = (#{type})NUM2ULONG(rb_funcall(v#{i}, #{conv[i]}, 0))"
       }
       ftncall = desc[:name] + "(" + desc[:args].size.enum.inject("") { |acc, i|
          acc + "p#{i}, "
@@ -219,11 +217,18 @@ EOS
                   end +
                   "#{ftncall})"
             end
-               
+     define_values = []
+     (0..desc[:args].size).map do |j|
+       define_values << "VALUE v#{j};"
+     end
+
       out.puts <<EOS
 VALUE #{rbname}(#{args.join(",")})
 {
+    #{define_values.join("\n    ") }
+    rb_scan_args(argc, argv, "#{desc[:args].size}*", #{(0..desc[:args].size).map{ |j| "&v#{j}" }.join(', ') });
     #{assign.join(" ;\n    ")} ;
+
     #{ret} ;
 }
 
@@ -256,7 +261,7 @@ end
 
 module Main
    extend self
-   
+
    def genheader(*args)
       GenerateHeader.write(*args)
    end
@@ -269,7 +274,7 @@ module Main
       db = FunctionDB.read
       Lapack_c.write(db)
    end
-   
+
    def config
       unless have_header("f2c.h") and
             have_library("blas") and
@@ -302,5 +307,3 @@ module Main
       create
    end
 end
-
-
